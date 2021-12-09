e35bf11e4226c05bb351b62412553d0da1511a1c
Tupo26 <66062373+Tupo26@users.noreply.github.com>
2021-12-09 14:32:46 +0200

    add locations

---
+++ routes/endpoints.js
@@ -0,0 +1,67 @@
+const express = require("express");
+let endpoints = express.Router();
+let autoincrement = 0;
+let db = [
+    { id: ++autoincrement, name: "Tuomas" },
+    { id: ++autoincrement, name: "Antti" },
+    { id: ++autoincrement, name: "Sanna" },
+];
+
+endpoints.use(express.json());
+
+let regex = new RegExp("/[a-z]/");
+let regex1 = /[a-z]/;
+
+// http://localhost:8080/endpoint/1
+endpoints.get("/:id([0-9]+)", (req, res) => {
+    console.log(req.params.id);
+
+    let id = Number(req.params.id);
+
+    let temp = null;
+
+    for (let end of db) {
+        if (end.id === id) {
+            temp = end;
+        }
+    }
+
+    let end = db.find((end) => end.id === id);
+
+    if (end) res.send(end);
+    else res.status(404).send({ msg: "No entry found with id: " + id });
+});
+
+// http://localhost:8080/endpoint/
+endpoints.get("/", (req, res) => {
+    res.send(db);
+});
+
+endpoints.post("/", (req, res) => {
+    let end = req.body;
+    end.id = ++autoincrement;
+    db.push(end);
+    res.status(201).send(end);
+});
+
+endpoints.delete("/:id([0-9]+)", (req, res) => {
+    let id = Number(req.params.id);
+    let newDB = [];
+
+    // for (let end of db) {
+    //     if (end.id !== id) {
+    //         newDB.push(end);
+    //     }
+    // }
+
+    newDB = db.filter((end) => end.id !== id);
+
+    if (newDB.length < db.length) {
+        db = newDB;
+        res.status(204).end();
+    } else {
+        res.status(404).send({ msg: "No entry found with id: " + id });
+    }
+});
+
+module.exports = endpoints;
---
+++ routes/locations.js
@@ -0,0 +1,55 @@
+const express = require("express");
+let locations = express.Router();
+
+let autoincrement = 0;
+let db = [
+    { id: ++autoincrement, latitude: 60, longitude: 60 },
+    { id: ++autoincrement, latitude: 40, longitude: 10 },
+    { id: ++autoincrement, latitude: 30, longitude: 70 },
+];
+
+locations.use(express.json());
+
+// http://localhost:8080/locations/
+locations.get("/", (req, res) => {
+    res.send(db);
+});
+
+// http://localhost:8080/locations/1
+locations.get("/:id([0-9]+)", (req, res) => {
+    console.log(req.params.id);
+
+    let id = Number(req.params.id);
+
+    let loc = db.find((loc) => loc.id === id);
+
+    if (loc) {
+        res.send(loc);
+    } else {
+        res.status(404).send({ msg: "No entry found with id: " + id });
+    }
+});
+
+locations.delete("/:id([0-9]+)", (req, res) => {
+    let id = Number(req.params.id);
+    let newDB = [];
+
+    newDB = db.filter((loc) => loc.id !== id);
+
+    if (newDB.length < db.length) {
+        db = newDB;
+        res.status(204).end();
+    } else {
+        res.status(404).send({ msg: "No entry found with id: " + id });
+    }
+});
+
+locations.post("/", (req, res) => {
+    let loc = req.body;
+    console.log(req.body);
+    loc.id = ++autoincrement;
+    db.push(loc);
+    res.status(201).send(loc);
+});
+
+module.exports = locations;
